c79e86f9be9e2ac9c6d0c5be823302a831eda03b
Tori Gushulak <tori.gushulak@gmail.com>
2023-04-14 13:40:56 -0600

    done

--- src/script.js
+++ src/script.js
@@ -1,6 +1,27 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
+import * as dat from 'dat.gui'
+
+
+// debug
+const gui = new dat.GUI();
+
+
+
+const parameters = {
+    color: 0xff0000,
+    // spin square
+    spin: () => {
+        gsap.to(mesh.rotation, {duration: 1, y: mesh.rotation.y + 10})
+    }
+}
+// change color of square
+gui.addColor(parameters, 'color').onChange(() => {
+    material.color.set(parameters.color)
+})
+
+gui.add(parameters, 'spin')
 
 /**
  * Base
@@ -15,9 +36,17 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ color: parameters.color })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+
+// debug / 
+gui.add(mesh.position, 'y').min(-3).max(3).step(0.01).name('elevation')
+
+gui.add(mesh, 'visible')
+gui.add(material, 'wireframe')
+
 
 /**
  * Sizes
